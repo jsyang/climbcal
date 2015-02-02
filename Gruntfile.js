2d148bcd089d76b060d7698ea9a3cb772fc84c9e
Jim Yang <jim@thewindow.com>
2015-02-02 22:09:07 +0000

    add minified build to heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,20 @@
-module.exports = function(grunt) {
-	"use strict";
+module.exports = function (grunt) {
+    "use strict";
 
-    var tasksConfig = grunt.file.readJSON( "GruntTasks.json" );
+    var tasksConfig = grunt.file.readJSON("GruntTasks.json");
 
-    tasksConfig["__loadNpmTasks__"].forEach(function(taskName){
+    tasksConfig["__loadNpmTasks__"].forEach(function (taskName) {
         grunt.loadNpmTasks(taskName);
     });
 
-	var config = { pkg: grunt.file.readJSON( "package.json" ) };
+    var config = { pkg: grunt.file.readJSON("package.json") };
 
-	for(var key in tasksConfig) {
-		config[key] = tasksConfig[key];
-	}
+    for (var key in tasksConfig) {
+        config[key] = tasksConfig[key];
+    }
 
-	grunt.initConfig(config);
-  grunt.registerTask( "default", [ "jshint", "clean", "copy:assets", "browserify", "extract_sourcemap", "concat"] );
+    grunt.initConfig(config);
+
+    grunt.registerTask("build-minified",  [ "jshint", "clean", "copy:assets", "browserify", "uglify", "extract_sourcemap", "concat"]);
+    grunt.registerTask("default", [ "jshint", "clean", "copy:assets", "browserify", "extract_sourcemap", "concat"]);
 };
