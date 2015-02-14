6049a99902232a5c5cbdb1f46e6704dddb5eea17
jsyang <jsyang.ca@gmail.com>
2015-02-14 20:05:11 +0000

    fixup the tracking code

--- app/util/tracking.js
+++ app/util/tracking.js
@@ -2,16 +2,16 @@ module.exports = function() {
     // Pageview tracking
     if (location.hostname !== 'localhost') {
         // Statcounter
-        var sc_project=10277940;
-        var sc_invisible=1;
-        var sc_security="11fc2066";
-        var scJsHost = (("https:" == document.location.protocol) ?
+        window.sc_project=10277940;
+        window.sc_invisible=1;
+        window.sc_security="11fc2066";
+        window.scJsHost = (("https:" == document.location.protocol) ?
         "https://secure." : "http://www.");
 
         var scriptTag = document.createElement('script');
         scriptTag.setAttribute('type', 'text/javascript');
-        scriptTag.setAttribute('src', scJsHost + "statcounter.com/counter/counter.js");
-        
+        scriptTag.setAttribute('src', window.scJsHost + "statcounter.com/counter/counter.js");
+
         document.head.appendChild(scriptTag);
     }
 };
